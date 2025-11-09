bdcb8a3ebdb80c3faead6186846378579676f0a8
aumthakkar <aumthakkar@yahoo.co.uk>
2025-11-09 20:54:53 +0000

    added py-app template

---
+++ 2py-app/template/src/${{values.app_name}}.py
@@ -0,0 +1,32 @@
+
+from flask import Flask, jsonify
+import socket
+import time
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/info')
+# ‘/’ URL is bound with hello_world() function.
+def info():
+    return jsonify({
+        "hostname" : socket.gethostname(),
+        "time" : time.ctime(),
+        "message" : "You are doing great Pranav! Way to go!! <3"
+        "env" : "${{values.app_env}}",
+        "app_name" : "${{values.app_name}}"
+    })
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({
+        "status" : "up"
+    })
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host="0.0.0.0")
